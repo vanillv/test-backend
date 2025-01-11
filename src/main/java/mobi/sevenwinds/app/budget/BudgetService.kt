cec7438fc9426e2be62471b2b3a11841ad5460bc
Ivan Masnev <vanillvanish@gmail.com>
2025-01-11 18:27:05 +0300

    Removed the "Комиссия" type and updated the database via database migration file note: previous commit only noted that the "testBudgetPagination" test method was fixed, but the logic update fixed both test methods that had issues, including "testStatsSortOrder"

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -2,7 +2,6 @@ package mobi.sevenwinds.app.budget
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -22,13 +21,15 @@ object BudgetService {
 
     suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
         transaction {
-            val total = BudgetTable.select { BudgetTable.year eq param.year }.count()
             val query = BudgetTable
                 .select { BudgetTable.year eq param.year }
-                .orderBy(BudgetTable.month to SortOrder.ASC, BudgetTable.amount to SortOrder.DESC)
                 .limit(param.limit, param.offset)
+
+            val total = query.count()
             val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
+
             val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
+
             return@transaction BudgetYearStatsResponse(
                 total = total,
                 totalByType = sumByType,
